fd4bc771a1e902d7b459c861da3419992f1fcd34
uhamhz <ammarhafizh2404@gmail.com>
2023-10-14 20:32:09 +0700

    Add Average Function

--- SCRIPT/ForKelipatan17.java
+++ SCRIPT/ForKelipatan17.java
@@ -10,6 +10,7 @@ public class ForKelipatan17 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int kelipatan, jumlah = 0, counter = 0,total = 0;
+        double rataRata;
 
         System.out.print("Masukan bilangan kelipatan (1-9) : ");
         kelipatan =  scan.nextInt();
@@ -22,5 +23,8 @@ public class ForKelipatan17 {
         }
         System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n",kelipatan,counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n",kelipatan,total);
+
+        rataRata = (double)counter / (double)kelipatan;
+        System.out.printf("Rata-Rata dari bilangan yang anda masukan adalah %f ", rataRata);
     }
 }
